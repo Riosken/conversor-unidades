dd236a153dd645e4be16971bc3df803486e31e27
yalesteran <teranyaleska@gmail.com>
2026-07-12 18:35:28 -0500

    Agrega conversion pies a metros

--- conversor.c
+++ conversor.c
@@ -5,9 +5,14 @@ float metrosAPies(float m){
     return m * 3.28084;
 }
 
+float piesAMetros(float p){
+    return p / 3.28084;
+}
+
 void menu(){
     printf("\n==== CONVERSOR DE UNIDADES ====\n");
     printf("1. Metros a Pies\n");
+    printf("2. Pies a Metros\n");
     printf("0. Salir\n");
     printf("Opcion: ");
 }
@@ -23,6 +28,11 @@ int main(){
                 printf("Metros: ");
                 scanf("%f", &val);
                 printf("%.2f m = %.2f pies\n", val, metrosAPies(val));
+                break;
+            case 2:
+                printf("Pies: ");
+                scanf("%f", &val);
+                printf("%.2f pies = %.2f m\n", val, piesAMetros(val));
                 break;
             case 0:
                 printf("Hasta luego!\n");
