d63c35318928f1345a339e08a504add18bc921d7
Riosken <ebahamonde481@gmail.com>
2026-07-12 18:24:34 -0500

    Agrega conversion Fahrenheit a Celsius

--- conversor.c
+++ conversor.c
@@ -5,9 +5,14 @@ float celsiusAFahrenheit(float c){
     return c * 9.0 / 5.0 + 32.0;
 }
 
+float fahrenheitACelsius(float f){
+    return (f - 32.0) * 5.0 / 9.0;
+}
+
 void menu(){
     printf("\n==== CONVERSOR DE UNIDADES ====\n");
     printf("1. Celsius a Fahrenheit\n");
+    printf("2. Fahrenheit a Celsius\n");
     printf("0. Salir\n");
     printf("Opcion: ");
 }
@@ -23,6 +28,11 @@ int main(){
                 printf("Grados Celsius: ");
                 scanf("%f", &val);
                 printf("%.2f C = %.2f F\n", val, celsiusAFahrenheit(val));
+                break;
+            case 2:
+                printf("Grados Fahrenheit: ");
+                scanf("%f", &val);
+                printf("%.2f F = %.2f C\n", val, fahrenheitACelsius(val));
                 break;
             case 0:
                 printf("Hasta luego!\n");
