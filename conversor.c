e6d537c838b43fd64733685fec11ed51b2f6052f
yalesteran <teranyaleska@gmail.com>
2026-07-12 18:34:33 -0500

    Agrega conversion metros a pies

--- conversor.c
+++ conversor.c
@@ -1,18 +1,29 @@
 #include <stdio.h>
 
 // Conversor de unidades - version inicial
+float metrosAPies(float m){
+    return m * 3.28084;
+}
+
 void menu(){
     printf("\n==== CONVERSOR DE UNIDADES ====\n");
+    printf("1. Metros a Pies\n");
     printf("0. Salir\n");
     printf("Opcion: ");
 }
 
 int main(){
     int op;
+    float val;
     do{
         menu();
         scanf("%d", &op);
         switch(op){
+            case 1:
+                printf("Metros: ");
+                scanf("%f", &val);
+                printf("%.2f m = %.2f pies\n", val, metrosAPies(val));
+                break;
             case 0:
                 printf("Hasta luego!\n");
                 break;
